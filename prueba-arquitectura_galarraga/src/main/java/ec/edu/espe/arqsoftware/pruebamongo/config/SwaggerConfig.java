87a41315b8547c7003ab366e0a04967fec44f364
María Fernanda Galárraga Molina <mfgalarraga1@espe.edu.ec>
2022-01-19 12:10:44 -0500

    Creación proyecto

---
+++ prueba-arquitectura_galarraga/src/main/java/ec/edu/espe/arqsoftware/pruebamongo/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.arqsoftware.pruebamongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ec.edu.espe.arqsoftware.pruebamongo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
---
+++ prueba-arquitectura_galarraga/src/test/java/ec/edu/espe/arqsoftware/pruebamongo/PruebamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqsoftware.pruebamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
